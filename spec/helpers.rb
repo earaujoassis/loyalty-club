bcc2835f67226303cf41eb433b976fca1656d377
Ewerton Araujo <earaujoassis@gmail.com>
2014-05-08 13:55:35 -0300

    update: just a typo/identation on Hectic::TestHelpers

--- spec/helpers.rb
+++ spec/helpers.rb
@@ -18,13 +18,13 @@ module Hectic
     def symbolize_keys(hash)
       hash.inject({}){|result, (key, value)|
         new_key = case key
-                  when String then key.to_sym
-                  else key
-                  end
+            when String then key.to_sym
+            else key
+          end
         new_value = case value
-                    when Hash then symbolize_keys(value)
-                    else value
-                    end
+            when Hash then symbolize_keys(value)
+            else value
+          end
         result[new_key] = new_value
         result
       }
