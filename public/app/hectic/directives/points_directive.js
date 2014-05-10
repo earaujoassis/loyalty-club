e40794d6ef89e727004f252ca906c107f219fc61
Ewerton Araujo <earaujoassis@gmail.com>
2014-05-09 23:47:44 -0300

    fix: an improved and DRY 'points' Directive

--- public/app/hectic/directives/points_directive.js
+++ public/app/hectic/directives/points_directive.js
@@ -2,6 +2,26 @@ define(["angular", "hectic"], function (angular) {
     "use strict";
 
     angular.module("app.directives").directive("points", function () {
+        var apply = function (scope, value, standard) {
+            if (value === 'undefined' && standard === 'undefined') {
+                return;
+            }
+
+            var values = [value, standard];
+            for (var i = 0, length = values.length; i < length; i += 1) {
+                var element = parseInt(values[i]);
+                if (element > 0) {
+                    scope.class = "positive";
+                    scope.value = "+" + element.toString();
+                    return;
+                } else if (element === 0) {
+                    scope.class = "nil";
+                    scope.value = "0";
+                    return;
+                }
+            }
+        };
+
         return {
             scope: {
                 value: "=",
@@ -12,56 +32,11 @@ define(["angular", "hectic"], function (angular) {
             template: '<span class="number {{ class }}">{{ value }}</span>',
             link: function (scope, elem, attrs) {
                 scope.$watch("value", function (newValue) {
-                    if (newValue === 'undefined' && scope.standard === 'undefined') {
-                        return;
-                    }
-
-                    newValue = parseInt(newValue);
-                    if (newValue > 0) {
-                        scope.class = "positive";
-                        scope.value = "+" + newValue.toString();
-                        return;
-                    } else if (newValue === 0) {
-                        scope.class = "nil";
-                        scope.value = "0";
-                        return;
-                    }
-
-                    scope.standard = parseInt(scope.standard);
-                    if (scope.standard > 0) {
-                        scope.class = "positive";
-                        scope.value = "+" + scope.standard.toString();
-                    } else if (scope.standard === 0) {
-                        scope.class = "nil";
-                        scope.value = "0";
-                    }
-
+                    return apply(scope, newValue, scope.standard);
                 });
             },
             controller: function ($scope) {
-                if ($scope.value === 'undefined' && $scope.standard === 'undefined') {
-                    return;
-                }
-
-                $scope.value = parseInt($scope.value);
-                if ($scope.value > 0) {
-                    $scope.class = "positive";
-                    $scope.value = "+" + $scope.value.toString();
-                    return;
-                } else if ($scope.value === 0) {
-                    $scope.class = "nil";
-                    $scope.value = "0";
-                    return;
-                }
-
-                $scope.standard = parseInt($scope.standard);
-                if ($scope.standard > 0) {
-                    $scope.class = "positive";
-                    $scope.value = "+" + $scope.standard.toString();
-                } else if ($scope.standard === 0) {
-                    $scope.class = "nil";
-                    $scope.value = "0";
-                }
+                return apply($scope, $scope.value, $scope.standard);
             }
         }
     });
